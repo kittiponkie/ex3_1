18a64f5fa2b371826c111df6a193814212a91b8d
6272 <a6272@6272s-Mac-mini.local>
2018-09-04 09:49:56 +0700

    Initial commit

---
+++ app/src/main/java/com/egco428/ex3/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.egco428.ex3
+
+import android.app.ListActivity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+class MainActivity : ListActivity() {
+
+    var mobileList = arrayOf("Samsung","Apple","Oppo","HTC","Asus","Vivo","Lenovo","LG")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,mobileList)
+
+        listAdapter = adapter
+    }
+}
